89cc4e6f2bba4da39c3aedcb778337f9aaf2e158
Ketki <113698124+KetkiMD@users.noreply.github.com>
2022-12-29 12:03:05 +0530

    Create Emoji.py
    To install module emoji , run following command:
    pip install emoji

---
+++ Emoji.py
@@ -0,0 +1,20 @@
+import emoji
+
+print(emoji.emojize(":grinning_face_with_big_eyes:"))
+print(emoji.emojize(":winking_face_with_tongue:"))
+print(emoji.emojize(":zipper-mouth_face:"))
+
+
+# Sample Emoji Using Unicodes
+print("\U0001F602")
+print("\U0001F92A")
+print("\U0001F92B")
+print("\U0001F605")
+print("\U0001F60D")
+
+
+# OR Using CLDR short name
+print("\N{grinning face}")
+print("\N{slightly smiling face}")
+print("\N{winking face}")
+
