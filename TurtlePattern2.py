c08d6767d7520381379e52d2296615e90d924e5d
Ketki <113698124+KetkiMD@users.noreply.github.com>
2022-12-31 14:17:34 +0530

    Create TurtlePattern2.py

---
+++ TurtlePattern2.py
@@ -0,0 +1,17 @@
+import turtle
+skk = turtle.Turtle()
+
+skk.speed(1)
+
+skk.color("pink")
+skk.fillcolor('pink')
+skk.begin_fill()
+skk.left(140)
+skk.forward(180)
+skk.circle(-90,200)
+skk.left(120)
+skk.circle(-90,200)
+skk.forward(180)
+
+
+skk.end_fill()
